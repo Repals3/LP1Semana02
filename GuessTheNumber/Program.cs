487efc93af181fcb981388422213b7d6bae20538
Diogo Coelho <diogocoelho.dev@gmail.com>
2025-02-28 17:45:00 +0000

    First commit

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace GuessTheNumber
 {
@@ -6,7 +7,40 @@ namespace GuessTheNumber
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Random rnd= new Random();
+            int numberToGuess = rnd.Next(31);
+
+
+            
+            bool cycle =true;
+            while (cycle);
+            (
+                Console.WriteLine("Insert number: ");
+                string num = Console.ReadLine();
+                int num1 = int.Parse(num);
+
+                if (num1 == numberToGuess)
+                {
+                Console.WriteLine("You found the hidden number " + numberToGuess);
+                cycle = false;
+                }
+                else if (num1 < 0)
+                {
+                Console.WriteLine("Numbers must be between 0 and 30. Try again") ;
+                }
+                else if (num1 > 30)
+                {
+                Console.WriteLine("Numbers must be between 0 and 30. Try again") ;
+                }
+                else if (num1 > numberToGuess)
+                {
+                Console.WriteLine("The hidden number is lower than " + num1 +". Try again") ;
+                }
+                else if (num1 < numberToGuess)
+                {
+                Console.WriteLine("The hidden number is higher than " + num1 +". Try again") ;
+                }
+            )
         }
     }
 }
