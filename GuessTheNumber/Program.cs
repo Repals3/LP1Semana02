b2cdb0f02b0229e2cb70da2e25ec2c185718df8b
Diogo Coelho <diogocoelho.dev@gmail.com>
2025-02-28 17:58:52 +0000

    final push

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -13,34 +13,36 @@ namespace GuessTheNumber
 
             
             bool cycle =true;
-            while (cycle);
-            (
+            while (cycle)
+            {
                 Console.WriteLine("Insert number: ");
                 string num = Console.ReadLine();
                 int num1 = int.Parse(num);
 
                 if (num1 == numberToGuess)
                 {
-                Console.WriteLine("You found the hidden number " + numberToGuess);
-                cycle = false;
+
+                    Console.WriteLine("You found the hidden number " + numberToGuess);
+                    cycle = false;
+
                 }
                 else if (num1 < 0)
                 {
-                Console.WriteLine("Numbers must be between 0 and 30. Try again") ;
+                    Console.WriteLine("Numbers must be between 0 and 30. Try again") ;
                 }
                 else if (num1 > 30)
                 {
-                Console.WriteLine("Numbers must be between 0 and 30. Try again") ;
+                    Console.WriteLine("Numbers must be between 0 and 30. Try again") ;
                 }
                 else if (num1 > numberToGuess)
                 {
-                Console.WriteLine("The hidden number is lower than " + num1 +". Try again") ;
+                    Console.WriteLine("The hidden number is lower than " + num1 +". Try again") ;
                 }
                 else if (num1 < numberToGuess)
                 {
-                Console.WriteLine("The hidden number is higher than " + num1 +". Try again") ;
+                    Console.WriteLine("The hidden number is higher than " + num1 +". Try again");
                 }
-            )
+            }
         }
     }
 }
